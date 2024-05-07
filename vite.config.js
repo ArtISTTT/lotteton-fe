54b94e973cd09200ed1500add85ee4ea15e1505c
ArtISTTT <artemis09035@gmail.com>
2024-05-07 15:49:11 +0400

    Update manifest

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import copy from 'rollup-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), basicSsl()],
-     build: {
-       outDir: './docs'
-     },
-   base: './'
-})
+  base: './',
+  build: {
+    outDir: './docs',
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'src/tonconnect-manifest.json', dest: 'docs' },
+          ],
+          verbose: true,
+          hook: 'writeBundle'
+        })
+      ]
+    },
+  }
+});
