d45d689bc48ffb0c4fa95f7f5722d62c50099e5d
ArtISTTT <artemis09035@gmail.com>
2024-05-22 16:11:35 +0400

    Ton wallet conenction

--- src/components/pages/start/index.vue
+++ src/components/pages/start/index.vue
@@ -17,17 +17,31 @@
 <script setup>
 import { useAppStore } from "@/stores";
 import { useRouter } from "vue-router";
+import { TonClient } from "@twa-dev/sdk";
 
 const store = useAppStore();
 const router = useRouter();
 
-const onConnectWallter = () => {
-    console.log("CONNECT");
-    store.user = {
-        address: "UQCq_4MKth7bmvzHrGneW6Dz_nlEbB_3Gr8yl46_IRRgVfYD",
-        skinId: "1",
-    };
-    router.push({ name: "home" });
+const onConnectWallter = async () => {
+    try {
+        const client = new TonClient({
+            endpoint: "https://toncenter.com/api/v2/jsonRPC",
+        });
+
+        const provider = await client.wallet.createWalletProvider();
+        const walletAddress = await provider.getAddress();
+
+        store.user = {
+            address: walletAddress,
+            skinId: "1",
+        };
+
+        console.log("ADDRESS: ", walletAddress);
+
+        router.push({ name: "home" });
+    } catch (e) {
+        alert("Could not connect TON wallet");
+    }
 };
 </script>
 
